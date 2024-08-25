4d2bc8c219999b5365fa90cf67e436da2be327da
Julia Grigoreva <julia_panina@mail.ru>
2024-08-26 02:10:40 +0300

    fix: func check brackets config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
   if (str.length % 2 !== 0) return false;
+
+   let array = [].concat(...bracketsConfig).join();
+    console.log(array);
+
+
 }

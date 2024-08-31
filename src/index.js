26f573704b04aac40c322a03891345cfc59a8f98
Julia Grigoreva <julia_panina@mail.ru>
2024-08-31 15:38:04 +0300

    fix: function check changed

--- src/index.js
+++ src/index.js
@@ -1,32 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  if (str.length % 2 !== 0) return false;
+  while (str.includes('()') || str.includes('[]') || str.includes('{}') || str.includes('||')){
+    str = str.replaceAll("()", "").replaceAll("[]", "").replaceAll("{}", "").replaceAll("||", "")
+}
+  return !str
+  // if (str.length % 2 !== 0) return false;
   
-    let array = [].concat(...bracketsConfig);
+  //   let array = [].concat(...bracketsConfig);
 
-    const bracketsNew = {};
+  //   const bracketsNew = {};
 
-    for (let i = 0; i < array.length; i += 2) {
-        bracketsNew[array[i+1]] = array[i];
-    }
+  //   for (let i = 0; i < array.length; i += 2) {
+  //       bracketsNew[array[i+1]] = array[i];
+  //   }
 
-    let stack = [];
+  //   let stack = [];
 
-    function isClosedBracket (char) {
-        let keysOfBrackets = [];
-        for (let key in bracketsNew) {
-          keysOfBrackets.push(key);
-        }
-        return keysOfBrackets.indexOf(char) > -1;
-    }
+  //   function isClosedBracket (char) {
+  //       let keysOfBrackets = [];
+  //       for (let key in bracketsNew) {
+  //         keysOfBrackets.push(key);
+  //       }
+  //       return keysOfBrackets.indexOf(char) > -1;
+  //   }
 
-    for (let i = 0; i < str.length; i++) {
-        const cur = str[i];
-        if (isClosedBracket(cur)) {
-            if (bracketsNew[cur] !== stack.pop()) return false;
-        } else {
-            stack.push(cur);
-        }
-    }
+  //   for (let i = 0; i < str.length; i++) {
+  //       const cur = str[i];
+  //       if (isClosedBracket(cur)) {
+  //           if (bracketsNew[cur] !== stack.pop()) return false;
+  //       } else {
+  //           stack.push(cur);
+  //       }
+  //   }
     
-    return stack.length === 0;
+  //   return stack.length === 0;
 }
